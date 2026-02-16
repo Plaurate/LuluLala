c7c3cf6f296db1e01dc9a8903047635e0ecbbd52
Plaurate <152171932+Plaurate@users.noreply.github.com>
2026-02-16 10:15:08 +0200

    Update app.py

--- EatThatBite/app.py
+++ EatThatBite/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")   
+    
 #Datu saglabāšana
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
@@ -79,3 +80,4 @@ def edit_name(id):
 if __name__ == "__main__":
 
     app.run(debug=True)
+
